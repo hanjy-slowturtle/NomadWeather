1a9890b90e2c052c2115f0bb982a02294a8e65c6
hanjy-slowturtle <h920915@gmail.com>
2021-12-25 04:11:22 +0900

    ~ #2.4 Layout System

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "black" }}></View>
+      <View style={{ flex: 2, backgroundColor: "red" }}></View>
+      <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 48,
-    color: "red",
-  },
-});
